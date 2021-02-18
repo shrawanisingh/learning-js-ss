1be2446f427c5cd706a10a2abb8e1a59c5c0d6fa
rohitsharmahub <76805822+rohitsharmahub@users.noreply.github.com>
2021-02-18 19:51:28 +0530

    filter

--- basic/script.js
+++ basic/script.js
@@ -1,1 +1,15 @@
+const arr = [1, 2, 3, 4, 5];
 
+const returnArr = arr.filter(function (eachElem) {
+  return eachElem === 9;
+});
+
+console.log(returnArr);
+
+const arr2 = ['Rohit', 'Dipu', 'Badmash'];
+
+const returnArr2 = arr2.filter(function (eachElem) {
+  return eachElem === 'Badmash';
+});
+
+console.log(returnArr2);
